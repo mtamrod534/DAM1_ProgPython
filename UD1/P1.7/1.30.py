528f1c813ed8392702c6a197ff5f3742054bf945
mtamrod534 <mtamrod534@g.educaand.es>
2023-10-11 18:13:28 +0200

    Good Ending 1.30

--- UD1/P1.7/1.30.py
+++ UD1/P1.7/1.30.py
@@ -20,6 +20,13 @@ while incremento<=0 or total<=0:
     incremento=int(input("Introduce el incremento: "))
     total=int(input("Introduce el total: "))
 
-while num<=total:
-    print(num, end=", ")
+print("Serie=>", end=" ")
+print(num,end="-")
+while num<total-1:
     num+=incremento
+    if num==total-1:
+        print(num, end="")
+    elif num!=total-1:
+        print(num, end="..")
+
+print(f"-{total}",end="")
