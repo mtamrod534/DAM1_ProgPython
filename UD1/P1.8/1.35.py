3b31c59babdc99509597f86a496a32551a795cd5
mtamrod534 <145799983+mtamrod534@users.noreply.github.com>
2023-10-16 06:54:30 +0200

    Update 1.35.py

--- UD1/P1.8/1.35.py
+++ UD1/P1.8/1.35.py
@@ -52,6 +52,38 @@
 
 # --------------------------------------------
 
+# PSEUDOCÓDIGO CORREGIDO
+
+# Inicio
+
+#	Escribe "Introduce dos números: "
+#	Lee n1
+#	Lee n2
+	
+#	opcion = 0
+
+#	Mientras (opcion < 1 or opcion > 4) hacer
+#		Escribe "Introduce una opción (1 = Suma / 2 = Resta / 3 = Multiplicación / 4 = División): "
+#		Lee opcion
+#		Si (opcion < 1 or opcion > 4) entonces
+#			Escribe "Error - No es una opción correcta (1-4)"
+	
+#	Si (opcion=1) entonces
+#		Escribe n1 + " + " + n2 + " = " + (n1+n2)
+#	Sino si (opcion=2) entonces
+#		Escribe n1 + " - " + n2 + " = " + (n1-n2)
+#	Sino si (opcion=3) entonces
+#		Escribe n1 + " * " + n2 + " = " + (n1*n2)
+#	Sino si (opcion=4) entonces
+#		Si (n2 != 0) entonces
+#			Escribe n1 + " / " + n2 + " = " + (n1/n2)
+#		Sino
+#			Escribe "La división por cero no es posible"			
+
+# Fin
+
+# --------------------------------------------
+
 n1=float(input("Introduce un número: "))
 n2=float(input("Introduce otro número: "))
 
