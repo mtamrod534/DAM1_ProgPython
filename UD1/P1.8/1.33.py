940010c553c374676ca47d37ea9df6bf99f7ae3d
mtamrod534 <145799983+mtamrod534@users.noreply.github.com>
2023-10-16 06:48:15 +0200

    Update 1.33.py

--- UD1/P1.8/1.33.py
+++ UD1/P1.8/1.33.py
@@ -39,6 +39,35 @@
 
 # --------------------------------------------
 
+# PSEUDOCÓDIGO CORREGIDO
+
+# Inicio
+
+#	Escribe "Dame 3 números: "
+#	Lee n1
+#	Lee n2
+#	Lee n3
+	
+#	Si (n1 < n2 and n1 < n3) entonces
+#		Si (n2 < n3) entonces
+#			Escribe n1 + " " + n2 + " " + n3
+#		Sino
+#			Escribe n1 + " " + n3 + " " + n2
+#	Sino si (n2 < n1 and n2 < n3) entonces
+#		Si (n1 < n3) entonces
+#			Escribe n2 + " " + n1 + " " + n3
+#		Sino
+#			Escribe n2 + " " + n3 + " " + n1
+#	Sino si (n3 < n1 and n3 < n2) entonces
+#		Si (n1 < n2) entonces
+#			Escribe n3 + " " + n1 + " " + n2
+#		Sino
+#			Escribe n3 + " " + n2 + " " + n1
+
+# Fin
+
+# --------------------------------------------
+
 n1=int(input("Introduce un número: "))
 n2=int(input("Introduce otro número: "))
 n3=int(input("Introduce otro número más: "))
