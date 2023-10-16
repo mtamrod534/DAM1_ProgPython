ead3a7de9312e7178892be21b0a2d16db16e9994
mtamrod534 <145799983+mtamrod534@users.noreply.github.com>
2023-10-16 06:56:39 +0200

    Update 1.36.py

--- UD1/P1.8/1.36.py
+++ UD1/P1.8/1.36.py
@@ -44,6 +44,34 @@
 
 # --------------------------------------------
 
+# PSEUDOCÓDIGO CORREGIDO
+
+# 	Escribe "¿Cuántas notas vas a introducir? "
+#	Lee total
+	
+#	Mientras (total < 1 o total > 100) hacer
+#		Escribe "Error - el número de notas debe ser un número entero entre 1 y 100"
+#		Escribe "¿Cuántas notas vas a introducir? "
+#		Lee total
+
+#	Escribe "Dame las " + total + " notas del curso: "
+	
+#	media = 0
+#	cont = 1
+
+#	Mientras (cont <= total) hacer
+#		Lee nota
+#		media = media + nota
+#		cont = cont + 1
+
+#	media = media / total
+
+#	Escribe "La media es " + media
+
+# Fin
+
+# --------------------------------------------
+
 total=int(input("¿Cuántas notas vas a introducir?: "))
 
 while (total<1) or (total>100):
